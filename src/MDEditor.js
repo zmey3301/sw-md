9be52689e0f271a50563c6ff9010781c91ed6c1b
SoulString <zmey3301@gmail.com>
2019-01-02 21:18:14 +0400

    Editor changing improvment
    add: observer 4 detecting current bb textarea status when changing XenForo editor mode
    cln: code
    cng: input styles

--- src/MDEditor.js
+++ src/MDEditor.js
@@ -4,19 +4,40 @@ import MDIcon from "./assets/images/markdown.svg"
 import createElement from "./utils/createElement"
 const buttonHTML =
 `<button type="button" role="button" tabindex="-1" data-cmd="XfMdCode" class="fr-btn fr-command fr-btn-font_awesome md__btn" title="Открыть редактор markdown">${MDIcon}</button>`
-const mdTextarea = "<textarea class='input md__input'></textarea>"
+const mdTextarea = "<textarea class='input md__textarea'></textarea>"
 export default class {
   id = uuid()
   #button = createElement(buttonHTML)
   #mdTextarea = createElement(mdTextarea)
+  #bbTextareaSelector = "textarea[name='message']"
   #bbModeButton = null
   #bbTextarea = null
   #opened = false
+  #openingEditor = false
   #needToChangeViewer = false
+  #observer = new MutationObserver((mutations) => {
+    // Observing current bb textarea status 4 better editor change
+    const openEditor = () => {
+      if (this.#openingEditor && !this.#bbTextarea.hasAttribute("disabled") && this.#bbTextarea.style.display !== "none") {
+        this.editorOpened = true
+        this.#openingEditor = false
+      }
+    }
+    for (const mutation of mutations) {
+      if (mutation.type === "childList") {
+        const bbTextarea = Array.from(mutation.addedNodes).find(element => element.matches(this.#bbTextareaSelector))
+        if (bbTextarea) {
+          this.#bbTextarea = bbTextarea
+          this.#observer.observe(bbTextarea, {attributes: true, attributeFilter: ["disabled", "style"]})
+          openEditor()
+        }
+      } else if (mutation.type === "attributes") openEditor()
+    }
+  })
 
   constructor (node) {
     const toolbar = node.querySelector(".fr-toolbar")
-    const bbTextareaSelector = "textarea[name='message']"
+    const inputWrapper = node.querySelector(".fr-box.bbWrapper")
 
     window.MDEditors = {[this.id]: this, ...window.MDEditors}
     node.setAttribute("data-md-editor", this.id)
@@ -28,12 +49,10 @@ export default class {
 
     this.#mdTextarea.style.display = "none"
     this.#mdTextarea.addEventListener("input", this.updateMain.bind(this))
-    node.querySelector(".fr-box.bbWrapper").appendChild(this.#mdTextarea)
+    inputWrapper.appendChild(this.#mdTextarea)
 
-    this.#bbTextarea = node.querySelector(bbTextareaSelector)
-    if (!this.#bbTextarea) this.#bbModeButton.addEventListener("mouseup", () => {
-      setTimeout(() => {this.#bbTextarea = node.querySelector(bbTextareaSelector)})
-    }, {once: true})
+    this.#bbTextarea = node.querySelector(this.#bbTextareaSelector)
+    if (!this.#bbTextarea) this.#observer.observe(inputWrapper, {childList: true})
   }
 
   get editorOpened () {
@@ -41,13 +60,12 @@ export default class {
   }
 
   set editorOpened (newValue) {
-    console.log("opened", newValue)
     this.#opened = newValue
     this.#button.classList.toggle("_active", newValue)
+    this.#bbModeButton.classList.toggle("mde-hard-disable", newValue)
     this.#mdTextarea.style.display = newValue ? "" : "none"
     this.#bbTextarea.style.display = newValue ? "none" : ""
     if (newValue) {
-      console.log(this.BB2MD(this.#bbTextarea.value))
       this.#mdTextarea.value = this.BB2MD(this.#bbTextarea.value)
     } else if (this.#needToChangeViewer) {
       this.toggleBBViewer()
@@ -59,8 +77,10 @@ export default class {
     const proceed = () => {this.editorOpened = !this.editorOpened}
     if (!this.editorOpened) {
       const changing = this.#needToChangeViewer = this.#bbTextarea?.hasAttribute("disabled") ?? true
-      if (changing) this.toggleBBViewer()
-      setTimeout(proceed)
+      if (changing) {
+        this.#openingEditor = true
+        this.toggleBBViewer()
+      } else proceed()
     } else proceed()
   }
 
@@ -84,10 +104,16 @@ export default class {
     text = basicSyntaxReplace(text, "U", "__")
     // Parsing line through
     text = basicSyntaxReplace(text, "S", "--")
-    // Parsing simple code
-    text = basicSyntaxReplace(text, "CODE", "`")
+    // Parsing align left
+    text = basicSyntaxReplace(text, "LEFT", "<<")
+    // Parsing align right
+    text = basicSyntaxReplace(text, "RIGHT", ">>")
+    // Parsing align center
+    text = basicSyntaxReplace(text, "CENTER", "^^")
     // Parsing highlighted code
     text = text.replace(/\[CODE=([^]+?)]([^]+?)\[\/CODE]/gim, "```$1\n$2```")
+    // Parsing simple code
+    text = basicSyntaxReplace(text, "CODE", "`")
     // Parsing img
     text = text.replace(/\[IMG]([^]+?)\[\/IMG]/gim, "!($1)")
     // Parsing link
@@ -108,6 +134,12 @@ export default class {
     text = basicSyntaxReplace(text, "U", "__")
     // Parsing line through
     text = basicSyntaxReplace(text, "S", "--")
+    // Parsing align left
+    text = basicSyntaxReplace(text, "LEFT", "<<")
+    // Parsing align right
+    text = basicSyntaxReplace(text, "RIGHT", ">>")
+    // Parsing align center
+    text = basicSyntaxReplace(text, "CENTER", "\\^\\^")
     // Parsing highlighted code
     text = text.replace(/`{3}([^]*?)\n([^]+?)`{3}/gm, "[CODE=$1]$2[/CODE]")
     // Parsing simple code
@@ -123,7 +155,6 @@ export default class {
   }
 
   destroy () {
-    console.log("destroyed")
     delete window.MDEditors[this.id]
     this.#button.removeEventListener("click", this.toggleEditor)
     this.#button.parentElement.removeChild(this.#button)
