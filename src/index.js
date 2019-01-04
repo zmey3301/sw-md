195cfb7384260241ac9109347018ed26693c15a8
SoulString <zmey3301@gmail.com>
2019-01-04 06:34:37 +0400

    Integration updates

--- src/index.js
+++ src/index.js
@@ -3,30 +3,48 @@ const username = document.querySelector(".p-navgroup-link--user > .p-navgroup-li
 function pageObserver () {
   console.log("init", username)
   const messageContentSelector = ".message-cell--main"
-  const messageObserver = new MutationObserver(mutations => {
+  const messageContainerSelector = ".js-replyNewMessageContainer"
+  const quickTreadSelector = ".js-quickThreadFields"
+  const observeMessage = node => observer.observe(node.querySelector(messageContentSelector), {attributes: true, attributeFilter: ["class"]})
+  const observer = new MutationObserver(mutations => {
     for (const mutation of mutations) {
-      if (mutation.target.classList.contains("is-editing"))
-        new MDEditor(mutation.target)
-      else {
-        window.MDEditors[mutation.target.getAttribute("data-md-editor")].destroy()
+      if (mutation.type === "attributes" && mutation.target.matches(messageContentSelector)) {
+        if (mutation.target.classList.contains("is-editing"))
+          new MDEditor(mutation.target)
+        else
+          window.MDEditors[mutation.target.getAttribute("data-md-editor")]?.destroy()
+      } else if (mutation.type === "childList") {
+        console.log(mutation)
+        if (mutation.target.matches(messageContainerSelector)) {
+          for (const node of mutation.addedNodes)
+            observeMessage(node)
+          for (const node of mutation.removedNodes)
+            observer.unobserve(node.querySelector(messageContentSelector))
+        } else if (mutation.target.matches(quickTreadSelector)) {
+          console.log(mutation.target.querySelector(".bbWrapper")?.length)
+          if (mutation.target.querySelector(".bbWrapper")?.length)
+            new MDEditor(mutation.target)
+          else
+            window.MDEditors[mutation.target.getAttribute("data-md-editor")]?.destroy()
+        }
       }
     }
   })
-  const observeMessage = node => messageObserver.observe(node.querySelector(messageContentSelector), {attributes: true, attributeFilter: ["class"]})
-  const listObserver = new MutationObserver(mutations => {
-    for (const mutation of mutations) {
-      for (const node of mutation.addedNodes)
-        observeMessage(node)
-      for (const node of mutation.removedNodes)
-        messageObserver.unobserve(node.querySelector(messageContentSelector))
+  if (["/conversations/", "/view/"].some(url => location.pathname.startsWith(url) && location.pathname !== url)) {
+    for (const container of Array.from(document.querySelectorAll(".js-replyNewMessageContainer"))) {
+      observer.observe(container, {childList: true})
+      for (const message of Array.from(container.querySelectorAll(`article.message[data-author="${username}"]`)))
+        observeMessage(message)
     }
-  })
-  for (const container of Array.from(document.querySelectorAll(".js-replyNewMessageContainer"))) {
-    listObserver.observe(container, {childList: true})
-    for (const message of Array.from(container.querySelectorAll(`article.message[data-author="${username}"]`)))
-      observeMessage(message)
+    for (const input of Array.from(document.querySelectorAll(".js-quickReply"))) new MDEditor(input)
   }
-  const quickReply = document.querySelectorAll(".js-quickReply")
-  for (const input of Array.from(quickReply)) new MDEditor(input)
+  if (location.pathname.startsWith("/pages/")) {
+    for (const node of Array.from(document.querySelectorAll(quickTreadSelector)))
+      observer.observe(node, {childList: true})
+    if (location.pathname.endsWith("/post-thread"))
+      new MDEditor(document.querySelector(".js-inlineNewPostFields"))
+  }
+  if (location.pathname === "/conversations/add")
+    new MDEditor(document.querySelector(".formRow--input.formRow--mergePrev"))
 }
 if (username) pageObserver()
